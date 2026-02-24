2f55422d1d5e4f7c37b316f966dd0a56860188a0
442294252 <442294252@qq.com>
2026-02-24 11:38:49 +0800

    Fix parameter formatting in SubPrepare.ps1

--- SubPrepare.ps1
+++ SubPrepare.ps1
@@ -1,8 +1,9 @@
 param(
     [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-    [string] $BuildDirectory,
+    [string]$BuildDirectory,
     [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-    [string] $BuildVersion
+    [string]$BuildVersion
 )
 
+# 替换格式图标（压缩文件显示的图标）
 Copy-Item -Force -Recurse -Path "FileIcons\*.ico" -Destination "$BuildDirectory\CPP\7zip\Archive\Icons"
