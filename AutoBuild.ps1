2cd83e537f96d77215f79b240e72fca08f1c2ce9
442294252 <442294252@qq.com>
2026-07-11 01:10:49 +0800

    Update AutoBuild.ps1

--- AutoBuild.ps1
+++ AutoBuild.ps1
@@ -1,29 +1,15 @@
 param(
-    [ValidateSet("official","zstd")]
-    [string] $BuildMode = "official
+    [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+    [string] $BuildVersion
 )
+
 $workDir = $PSScriptRoot
-if ($BuildMode -eq "official") {
-    $api = "https://api.github.com/repos/ip7z/7zip/releases/latest"
-} else {
-    $api = "https://api.github.com/repos/mcmilk/7-Zip-zstd/releases/latest"
-}
-$release = Invoke-RestMethod -Uri $api
-$fullTag = $release.tag_name
-if ($BuildMode -eq "official") {
-    $verNum = $fullTag -replace '\.', ''
-    $prefix = "7z$verNum"
-    & "$workDir\Prepare.ps1" -BuildVersion $prefix
-    $exeOut = "$prefix-Custom-Icon.exe"
-} else {
-    $verBase = $fullTag -split "-v" | Select-Object -First 1
-    $verNum = $verBase.TrimStart('v') -replace '\.', ''
-    $prefix = "7z$verNum"
-    & "$workDir\PrepareZstd.ps1" -RawTag $fullTag
-    $exeOut = "$prefix-ZS-Custom-Icon.exe"
-}
-# 编译打包共用
-& "$workDir\Build.ps1" -BuildVersion $prefix
-& "$workDir\Pack.ps1" -BuildVersion $prefix
-Rename-Item "$prefix.exe" $exeOut -Force
-Write-Host "✅ 构建完成，输出：$exeOut"
+
+# 下载源代码并替换资源文件
+& "$workDir\Prepare.ps1" $BuildVersion -ErrorAction Stop
+
+# 自动查找Visual Studio工具链并进行编译
+& "$workDir\Build.ps1" $BuildVersion -ErrorAction Stop
+
+# 导出编译好的文件并制作安装包
+& "$workDir\Package.ps1" $BuildVersion -ErrorAction Stop
